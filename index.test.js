32411b3b4f8ccd63d927202cbe2648aa4afe4bf0
Hamed Ali <123hamed@gmail.com>
2022-06-23 18:53:24 +0100

    add some more failing tests

--- index.test.js
+++ index.test.js
@@ -21,4 +21,31 @@ describe("applyCallback", () => {
 
     expect(result).toEqual([2, 4, 6]);
   });
+
+  it("It should return [2,3,4] when supplied with + function", () => {
+    const additionFunction = function (num) {
+      return num + 2;
+    };
+    const result = applyCallback([1, 2, 3], additionFunction);
+
+    expect(result).toEqual([2, 3, 4]);
+  });
+
+  it("It should return [.5, 1, 1.5] when supplied with / function", () => {
+    const divisionFunction = function (num) {
+      return num / 2;
+    };
+    const result = applyCallback([1, 2, 3], divisionFunction);
+
+    expect(result).toEqual([0.5, 1, 1.5]);
+  });
+
+  it("It should return [-1, 0, 1] when supplied with - function", () => {
+    const divisionFunction = function (num) {
+      return num - 2;
+    };
+    const result = applyCallback([1, 2, 3], divisionFunction);
+
+    expect(result).toEqual([-1, 0, 1]);
+  });
 });
